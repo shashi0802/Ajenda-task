cd7eb2ad5cb6f25ad7ed901e86c6f13944134a6c
shashi0802 <shashikant.yadav@contentstack.com>
2018-09-12 18:16:25 +0530

    third commit

--- js/main.js
+++ js/main.js
@@ -3,26 +3,22 @@ $(document).ready(function () {
         $('.detail').empty();
         var $popdata = $(this).siblings('.detail-content').clone();
         $('.detail').html($popdata);
-        $(".detail").addClass("active");
+        $(".detail").fadeIn();
         $("body").css({"overflow":"hidden"});
         console.log($popdata);
     });
 
     $(document).on('click', '.close', function () {
         console.log("click")
-        $('.detail').removeClass("active");
+        $('.detail').fadeOut();
         $("body").css({"overflow":"scroll"});
-    });
-
-    $(document).on('click', 'body', function () {
-        $(this).removeClass('active');
     });
 
     $(document).click(function (e) {
         if (!$(e.target).hasClass("activity-name") 
             && $(e.target).parents(".detail").length === 0) 
         {
-            $(".detail").removeClass("active");
+            $(".detail").fadeOut();
             $("body").css({"overflow":"scroll"});
         }
     });
